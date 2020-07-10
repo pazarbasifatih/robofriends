c4caab20b39a568ef6b2b04afaf17b83a32923de
pazarbasifatih <42507858+pazarbasifatih@users.noreply.github.com>
2020-07-10 03:02:21 +0300

    committing

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from "../components/ErrorBoundry";
 import './App.css';
 
 class App extends Component {
@@ -34,8 +35,8 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
-            <CardList robots={filteredRobots} />
+          <Scroll> <ErrorBoundry>
+            <CardList robots={filteredRobots} /></ErrorBoundry>
           </Scroll>
         </div>
       );
